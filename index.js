f6ae54a72e71b4a18765137c82b24082a2293f2c
SluvsP <salomekbg@gmail.com>
2016-11-19 18:17:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much.";
+const React = require('react');
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      hasBeenClicked: true
+    });
+  }
+
+  render() {
+    return (
+      hi
+      // <div>
+      //   <p>I have {this.state.hasBeenClicked ? 'not' : null} been clicked yet!</p>
+      //   <button onClick={this.handleClick}>Click me!</button>
+      // </div>
+    );
+  }
+}
+
+module.exports = ClickityClick;
